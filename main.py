aca45fcdc7603e052b10c8644adca680a0a3c021
aduuuu213 <aduuuu213@gmail.com>
2024-11-13 17:54:11 +0800

    1

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from PySide2.QtWidgets import QApplication
 from utils.config import ConfigManager
 from utils.install import install_plugin_update
 
-# os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'C:\Users\杜昌贵\.conda\envs\python38_32\Lib\site-packages\PySide2\plugins'  #### 这一行是新增的。用的是相对路径。
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(os.path.dirname(__file__), 'Lib', 'site-packages', 'PySide2', 'plugins')
 
 def main():
